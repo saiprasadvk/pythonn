90a3983389882bfa5ed7b7f00ec16fbc617c358f
saiprasadvk <67091926+saiprasadvk@users.noreply.github.com>
2020-07-22 10:23:42 +0530

    Create 3. 2nd biggest number.py

---
+++ 3. 2nd biggest number.py
@@ -0,0 +1,21 @@
+3. Find the 2nd biggest number from the list 
+  - I/o will be non sequence without sorting need to find the number 
+  - Dont use inbuild functionality 
+  - can use any looping functionality i/o - [20,10,23,50,600,300] o/p - 300
+  
+  Ans::
+  
+  
+I = [20,10,23,50,600,300]
+
+for i in range(len(I)):
+    for j in range(i + 1, len(I)):
+        if I[i] < I[j]:
+            I[i], I[j] = I[j], I[i]
+
+print(I[1])
+
+O/p::
+
+300
+  
