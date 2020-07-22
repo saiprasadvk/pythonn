31f8a541ca301fd656eea70fa151730054f8a115
saiprasadvk <67091926+saiprasadvk@users.noreply.github.com>
2020-07-22 10:31:19 +0530

    Create 9. Duplicate value dict.py

---
+++ 9. Duplicate value dict.py
@@ -0,0 +1,16 @@
+9. IF dictionary has duplicate value then group the keys and formed new dictionary i/o = {'a':1,'b':1,'c':1,'d'2} o/p - {('a','b','c'):1,'d':2}
+
+
+Ans:
+
+ip = {'a':1,'b':1,'c':1,'d':2}
+set_ip = set(ip.values())
+
+d = {}
+for n in set_ip:
+    d[n] = [k for k in ip.keys() if ip[k] == n]
+print(d)
+
+O/P:
+
+{1: ['a', 'b', 'c'], 2: ['d']}
