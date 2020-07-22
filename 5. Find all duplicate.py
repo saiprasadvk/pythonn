88e1cfc9d65c169d413305a6e60f11e6191b7f2e
saiprasadvk <67091926+saiprasadvk@users.noreply.github.com>
2020-07-22 16:10:14 +0530

    Update 5. Find all duplicate.py

--- 5. Find all duplicate.py
+++ 5. Find all duplicate.py
@@ -1,9 +1,10 @@
 5. Find all duplicate and their count from the list 
   - Should not used any inbuild functionalities (like counter) i/o - [1,1,1,2,2,3,4,5,6] o/p - 1 is 3 times 2 is 2 times
   
-  Ans::
+ Ans::
+sol1:
   
-  I = [1,1,1,2,2,3,4,5,6]
+ I = [1,1,1,2,2,3,4,5,6]
 one = 0
 two = 0
 three = 0
@@ -27,6 +28,25 @@ for x in I:
         
 print("1 is",one, "times")
 print("2 is",two, "times")
+
+sol2:
+  
+listOfElems = [1,1,1,2,2,3,4,5,6]
+
+def getDuplicatesWithCount(listOfElems):
+    dictOfElems = dict()
+    for elem in listOfElems:
+        if elem in dictOfElems:
+            dictOfElems[elem] += 1
+        else:
+            dictOfElems[elem] = 1
+            
+    dictOfElems = { key:value for key, value in dictOfElems.items() if value > 1}
+    return dictOfElems
+
+dictOfElems = getDuplicatesWithCount(listOfElems)     
+for key, value in dictOfElems.items():
+        print(key , 'is', value, "times")
 
 O/P:
 
