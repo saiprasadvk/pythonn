8399917357991635cd980a270d10f7debf74ba90
saiprasadvk <67091926+saiprasadvk@users.noreply.github.com>
2020-07-22 16:13:02 +0530

    Update 7. Remove the character.py

--- 7. Remove the character.py
+++ 7. Remove the character.py
@@ -3,7 +3,7 @@ i/o - "Python" o/p - "yhn"
 
 
 Ans:
-
+Sol1::
 I = "Python"
 O = ""
 
@@ -12,6 +12,11 @@ for i in range(1,len(I)+1):
         O += str(I[i-1])
         
 print(O)
+
+Sol2::
+        
+I = "Python"
+I[1::2]
 
 O/P:
 
