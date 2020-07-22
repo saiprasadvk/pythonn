ceebb4a95406e394480906ea11d2284d07203995
saiprasadvk <67091926+saiprasadvk@users.noreply.github.com>
2020-07-22 16:46:20 +0530

    Update 11. Longest word.py

--- 11. Longest word.py
+++ 11. Longest word.py
@@ -1,7 +1,8 @@
 11. Find the longest word from the sentense i/o - "This is the sample string program" o/p - "program" - 7 (longest word)
 
 Ans::
-
+sol1::
+        
 I = "This is the sample string program"
 
 m = I.split()
@@ -15,6 +16,22 @@ for x in m:
 for y in m:
     if n[-1] == len(y):
         print(y)
+        
+sol2:
+I = "This is the sample string program"
+
+m = I.split()
+
+max_len = -1
+
+for ele in m:
+    if len(ele) > max_len:
+        max_len = len(ele)
+        long = ele 
+
+print("The longest word is : ", long) 
+
+   
         
 O/P:
 
