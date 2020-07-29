5c6dd573a435f1c1313775d1a2b364732a215eb2
saiprasadvk <67091926+saiprasadvk@users.noreply.github.com>
2020-07-29 16:36:43 +0530

    Create two arguments and print its value.py

---
+++ datastructure/function/two arguments and print its value.py
@@ -0,0 +1,14 @@
+Create a function that can accept two arguments name and age and print its value
+
+Ans::
+
+def details(name,age):
+    print("name:",name)
+    print("age:",age)
+    
+details('sai',0)
+
+O/P:
+
+name: sai
+age: 0
