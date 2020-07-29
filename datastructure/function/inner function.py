840625ed6351d50392f603ec16e01f5df4d1e9c8
saiprasadvk <67091926+saiprasadvk@users.noreply.github.com>
2020-07-29 16:42:36 +0530

    Create inner function.py

---
+++ datastructure/function/inner function.py
@@ -0,0 +1,16 @@
+Create an inner function to calculate the addition in the following way
+
+Ans::
+
+def Sum(a,b):
+    pass
+    def Add(a,b):
+        c = a+b
+        return c
+    I = Add(a,b)
+    return I + 5
+
+Sum(2,3)
+
+O/P:
+10
