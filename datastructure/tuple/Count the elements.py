98caf602f86679a64009e7935db056d315c7c223
saiprasadvk <67091926+saiprasadvk@users.noreply.github.com>
2020-07-30 15:52:59 +0530

    Update Count the elements.py

--- datastructure/tuple/Count the elements.py
+++ datastructure/tuple/Count the elements.py
@@ -1,7 +1,7 @@
 Count the elements in a list until an element is a Tuple
 
 Ans::
-
+Sol1::
 #I = [4, 5, 6, 10, (1, 2, 3), 11, 2, 4]
 I = [4, (5, 6), 10, (1, 2, 3), 11, 2, 4]
 
@@ -12,7 +12,19 @@ for x in I:
     elif type(x) == tuple:
         print(I.index(x))
         break
+Sol2::
+        
+element=0
+
+for x in I:
+    if type(x) is tuple:
+        break
+    element+=1    
     
+    
+print(element)
+        
+        
 O/P::
 
 1
