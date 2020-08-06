c8a18066057dbf39133e5d6f0d0c38a3cae2cfff
saiprasadvk <67091926+saiprasadvk@users.noreply.github.com>
2020-08-06 16:15:12 +0530

    Create Static class.py

---
+++ oops/Static class.py
@@ -0,0 +1,38 @@
+1)
+class Mathematics:
+
+    def addNumbers(x, y):
+        return x + y
+
+# create addNumbers static method
+Mathematics.addNumbers = staticmethod(Mathematics.addNumbers)
+
+print('The sum is:', Mathematics.addNumbers(5, 10))
+
+O/P::
+The sum is: 15
+
+2)
+class Dates:
+    def __init__(self, date):
+        self.date = date
+        
+    def getDate(self):
+        return self.date
+
+    @staticmethod
+    def toDashDate(date):
+        return date.replace("/", "-")
+
+date = Dates("15-12-2016")
+dateFromDB = "15/12/2016"
+dateWithDash = Dates.toDashDate(dateFromDB)
+
+if(date.getDate() == dateWithDash):
+    print("Equal")
+else:
+    print("Unequal")
+    
+O/P::
+
+Equal
