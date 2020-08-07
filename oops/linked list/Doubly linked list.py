2e7c9f393c7f4f9f68450efa327730fade838902
saiprasadvk <67091926+saiprasadvk@users.noreply.github.com>
2020-08-07 16:17:43 +0530

    Create Doubly linked list.py

---
+++ oops/linked list/Doubly linked list.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class doubly_linked_list:
+    def __init__(self):
+        self.head = None
+
+# Adding data elements
+    def push(self, NewVal):
+        NewNode = Node(NewVal)
+        NewNode.next = self.head
+        if self.head is not None:
+            self.head.prev = NewNode
+        self.head = NewNode
+        
+        
+# Print the Doubly Linked list
+    def listprint(self, node):
+        while (node is not None):
+            print(node.data)
+            last = node
+            node = node.next
+
+l = doubly_linked_list()
+l.head = Node('1')
+
+l2 = Node('2')
+l3 = Node('3')
+
+l.head.next = l2
+l2.next  = l3
+
+l.push('14')
+l.push('13')
+l.push('12')
+
+l.listprint(l.head)
+
+O/P:
+
+12
+13
+14
+1
+2
+3
