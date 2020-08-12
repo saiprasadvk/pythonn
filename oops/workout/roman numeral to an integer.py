b263b246fd6fb1477895ce2d3245e55bd6a2bd1a
saiprasadvk <67091926+saiprasadvk@users.noreply.github.com>
2020-08-12 17:15:00 +0530

    Create roman numeral to an integer.py

---
+++ oops/workout/roman numeral to an integer.py
@@ -0,0 +1,19 @@
+class py_solution:
+    def roman_to_int(self, s):
+        rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        int_val = 0
+        for i in range(len(s)):
+            #print(rom_val[s[i]],"and",rom_val[s[i - 1]])
+            if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
+                int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+            else:
+                int_val += rom_val[s[i]]
+        return int_val
+    
+x = "IV"
+obj = py_solution()
+obj.roman_to_int(x)
+
+Ans::
+
+4
