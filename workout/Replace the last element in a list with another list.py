3dbc655fbd4d07b9d24989af2b207a11e49057bc
saiprasadvk <67091926+saiprasadvk@users.noreply.github.com>
2020-08-17 18:25:47 +0530

    Create Replace the last element in a list with another list.py

---
+++ workout/Replace the last element in a list with another list.py
@@ -0,0 +1,17 @@
+Write a Python program to replace the last element in a list with another list. 
+Sample data : [1, 3, 5, 7, 9, 10], [2, 4, 6, 8] 
+Expected Output: [1, 3, 5, 7, 9, 2, 4, 6, 8]
+
+Ans::
+
+num1 =  [1, 3, 5, 7, 9, 10]
+num2 = [2, 4, 6, 8]
+
+num1 = [1, 3, 5, 7, 9, 10]
+num2 = [2, 4, 6, 8]
+num1[-1:] = num2
+print(num1)
+
+O/P::
+
+[1, 3, 5, 7, 9, 2, 4, 6, 8]
